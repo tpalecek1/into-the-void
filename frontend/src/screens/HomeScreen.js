38c0b1a44a585367d317026632177addc76c81d6
tpalecek1 <36023933+tpalecek1@users.noreply.github.com>
2021-11-16 16:17:59 -0700

    prepare frontend for deployment

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import {Form, Button, Container, Row, Col} from 'react-bootstrap'
+import {Animate} from 'react-simple-animate'
 
 export const HomeScreen = () => {
     const [formText, setFormText] = useState('')
     const [animatedText, setAnimatedText] = useState('')
     const [submitted, setSubmitted] = useState(false)
     const [showButton, setShowButton] = useState(true)
+    const [play, setPlay] = useState(false)
 
     
     const submitHandler = (e) => {
@@ -14,7 +16,7 @@ export const HomeScreen = () => {
     }
     useEffect(() => {
         if(!showButton){
-            setTimeout(() => setShowButton(true), 2000)
+            setTimeout(() => {setShowButton(true); setPlay(false)}, 2100)
         }
         if(submitted){
             console.log('submitted loop')
@@ -22,20 +24,27 @@ export const HomeScreen = () => {
             setFormText('')
             setShowButton(false)
             setSubmitted(false)
+            setPlay(true)
         }
-    }, [submitted, showButton])
+    }, [submitted, showButton, formText])
 
     return (
         <Container>
-            <Form onSubmit={submitHandler} style={{position: 'fixed', top: '70%', left: '50%', transform: 'translate(-50%, -50%)'}}>
-                {showButton ? <><Form.Group className="mb-3" controlId="text">
-                    <Form.Control type="text" placeholder=". . ." onChange={e => setFormText(e.target.value)} value={formText}/>
+            <Form onSubmit={submitHandler} style={{position: 'fixed', top: '60%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+                <Animate duration={.5} play={play} start={{transform: 'translateY(0px)'}} end={{transform: 'translateY(-100px'}}>
+                    <Animate play={play} duration={1} delay={.5}  start={{opacity: 1, transform: 'scale(2)'}} end={{opacity: 0, transform: 'scale(.1)'}}>
+                        <h1 style={{
+                            color: 'white',
+                            visibility: showButton ? 'hidden' : 'visible'
+                            }}>{animatedText}</h1>
+                    </Animate>
+                </Animate>
+                {showButton && <><Form.Group className="mb-3" controlId="text">
+                    <Form.Control autoComplete="off" type="text" placeholder="Enter text here. . ." onChange={e => setFormText(e.target.value)} value={formText}/>
                 </Form.Group>
                 <Button style={{width: '100%'}} variant="dark" type="submit">
                     Send into the void
-                </Button></> : (
-                    <h1 style={{color: 'white'}}>{animatedText}</h1>
-                )}
+                </Button></>}
             </Form>
         </Container>
     )
